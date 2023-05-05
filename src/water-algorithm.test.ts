0a89e91eb47d42882e585a71946fbadd0d6ec72e
Audrow Nash <audrow@hey.com>
2023-05-04 21:17:56 -0500

    Add testing setup
    Signed-off-by: Audrow Nash <audrow@hey.com>

---
+++ src/water-algorithm.test.ts
@@ -0,0 +1,7 @@
+import { daysUntilWatering } from "./water-algorithm"
+
+describe("daysUntilWatering", () => {
+  it("returns 7", () => {
+    expect(daysUntilWatering()).toEqual(7)
+  })
+})
---
+++ src/water-algorithm.ts
@@ -0,0 +1,3 @@
+export function daysUntilWatering() {
+  return 7
+}
