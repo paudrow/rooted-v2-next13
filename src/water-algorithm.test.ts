ce53dbed6afa66b884e24957549fdbff21095180
Audrow Nash <audrow@hey.com>
2023-05-05 12:36:31 -0500

    Add get days between
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- src/water-algorithm.test.ts
+++ src/water-algorithm.test.ts
@@ -1,7 +1,41 @@
-import { daysUntilWatering } from "./water-algorithm"
+import { daysUntilWatering, getDaysBetweenDates } from "./water-algorithm"
 
 describe("daysUntilWatering", () => {
   it("returns 7", () => {
     expect(daysUntilWatering()).toEqual(7)
+  })
+})
+
+describe("getDaysBetweenDates", () => {
+  it.each([
+    {
+      dateStrings: [],
+      expected: [],
+    },
+    {
+      dateStrings: ["2021-01-01"],
+      expected: [],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-01"],
+      expected: [0],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-02"],
+      expected: [1],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-02", "2021-01-03"],
+      expected: [1, 1],
+    },
+    {
+      dateStrings: ["2021-01-01", "2021-01-02", "2021-01-03", "2021-01-06"],
+      expected: [1, 1, 3],
+    },
+  ])("%j", ({ dateStrings, expected }) => {
+    const dates = dateStrings.map(
+      (dateString) => new Date(Date.parse(dateString))
+    )
+    expect(getDaysBetweenDates(...dates)).toEqual(expected)
   })
 })
--- src/water-algorithm.ts
+++ src/water-algorithm.ts
@@ -1,3 +1,27 @@
+import dayjs from "dayjs"
+
+export function getDaysBetweenDates(...dates: Date[]): number[] {
+  const sortedDates = dates.sort((a, b) => a.getTime() - b.getTime())
+  const daysBetweenDates = []
+  for (let i = 0; i < sortedDates.length - 1; i++) {
+    const diff = dayjs(sortedDates[i + 1]).diff(sortedDates[i], "day")
+    daysBetweenDates.push(diff)
+  }
+  return daysBetweenDates
+}
+
 export function daysUntilWatering() {
   return 7
+}
+
+if (require.main === module) {
+  const dates: Date[] = [
+    new Date(Date.parse("2021-01-01")),
+    new Date(Date.parse("2021-01-02")),
+    new Date(Date.parse("2021-01-03")),
+    new Date(Date.parse("2021-01-04")),
+    new Date(Date.parse("2021-01-06")),
+  ]
+
+  console.log(getDaysBetweenDates(...dates))
 }
